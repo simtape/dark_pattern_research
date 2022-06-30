bac27a144b54f160e7a9e587812ceba60da07267
simtape <sim1cassetta@gmail.com>
2022-06-30 15:55:09 +0200

    init commit

---
+++ CookieBanner.py
@@ -0,0 +1,74 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebElement
+
+class CookieBanner:
+
+	def __init__(self, url, web_driver: webdriver):
+		self.web_driver = web_driver
+		self.url: url
+		self.approve_btn = None
+		self.reject_btn = None
+		self.more_btn = None
+
+
+	def spot_banner_buttons(self):
+		approve_word_keys = [
+			"accetto",
+			"sono d'accordo",
+			"accetta",
+			"accetta tutto",
+			"ok",
+			"accetta tutti"
+			"csdcever"
+		]
+
+		deny_word_keys = [
+			# "rifiuta",
+			# "non sono d'accordo"
+			"freverv"
+		]
+		approve_btn = self.spot_a_btn(approve_word_keys)
+		deny_btn = self.spot_a_btn(deny_word_keys)
+
+		if approve_btn:
+			approve_btn = BannerElement(approve_btn);
+			approve_btn.screenshot()
+
+		if deny_btn:
+			print("Trovato rifiuta")
+
+
+	def spot_a_btn(self, word_keys):
+
+		# looking for a button, an input field or a link
+		# since web sites banner not only use buttons
+		# for accept/reject cookie
+		elements_type = ["button", "input", "a"]
+		for type in elements_type:
+			elements = self.web_driver.find_elements(By.TAG_NAME, type)
+
+			for element in elements:
+
+				element_text = element.text or element.get_attribute("value")
+				if not element_text:
+					continue
+				element_text = "".join(char for char in element_text if char.isalpha())
+				element_text = "".join(element_text.lower().split())
+
+
+				for word_key in word_keys:
+					if word_key in element_text:
+						return element
+
+
+		return None
+
+
+class BannerElement:
+	def __init__(self, element: WebElement):
+		self.element = element
+
+	def screenshot(self):
+		self.element.screenshot("screenshots/pic1.png")
+
---
+++ main.py
@@ -0,0 +1,87 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import *
+import csv
+from CookieBanner import CookieBanner, BannerElement
+
+SLEEP_TIME_CMP_WAIT = 3
+
+
+# def executeCmpFunction(it_list):
+#     csvreader = csv.reader(it_list)
+#     options = Options()
+#     options.add_argument("--headless")
+#     driver = webdriver.Chrome(options=options)
+#     visitedwebsites = []
+#
+#     for website_name in csvreader:
+#         url = "https://" + website_name
+#         driver.get(url)
+#         time.sleep(SLEEP_TIME_CMP_WAIT)
+#
+#         try:
+#             driver.execute_script('if (__tcf) return "ok";', None)
+#             print('__tcf found')
+#             cookie_banner = CookieBanner(url, driver)
+#             cookie_banner.spot_banner_buttons()
+#             #website.tcfexists = True
+#         except JavascriptException as e:
+#             print('__tcf not found: ', e)
+#
+#     print(visitedwebsites)
+
+def checkTCFandFindButtons():
+	options = Options()
+
+	options.add_argument("--headless")
+	driver = webdriver.Chrome(options=options)
+	#visitedwebsites = []
+
+	url = "https://" + "repubblica.it"
+	driver.get(url)
+	time.sleep(SLEEP_TIME_CMP_WAIT)
+
+	try:
+		driver.execute_script('if (__tcfapi) return "ok";', None)
+		print('__tcf found')
+		cookie_banner = CookieBanner(url, driver)
+		cookie_banner.spot_banner_buttons()
+	# website.tcfexists = True
+	except JavascriptException as e:
+		print('__tcf not found: ', e)
+
+
+
+
+# takes as input the tranco list with the top 1M websites
+# and creates a csv with only italian domain web sites
+def filterList(tranco_list):
+	itwebsitefile = 'itwebsites.csv'
+	csvreader = csv.reader(tranco_list)
+	italianwebsites = []
+
+	try:
+		for row in csvreader:
+			str = row[1]
+			if str.endswith('.it'):
+				italianwebsites.append(str)
+
+		try:
+			with open(itwebsitefile, 'w', newline='') as fl:
+				writer = csv.writer(fl)
+				for website in italianwebsites:
+					writer.writerow([website])
+
+		except BaseException as e:
+			print('BaseException:', fl)
+
+	except BaseException as e:
+		print('BaseException:', tranco_list)
+
+
+# tranco_list = open('tranco_25JP9.csv')
+# filterList(tranco_list)
+
+checkTCFandFindButtons()
+# executeCmpFunction(itwebsites)
