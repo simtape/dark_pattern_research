3f18557e3f66222dc6881a2db2f65fa77d99f6c0
martinaburo <m.buro2@studenti.unimol.it>
2022-08-31 15:23:08 +0200

    Button detection and database connection

--- CookieBanner.py
+++ CookieBanner.py
@@ -1,80 +1,288 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebElement
+import json
+import sys
+import os
+import splinter
+import selenium
+from loguru import logger as log
+from splinter import Browser as Sbrowser
+from selenium import webdriver as wd
+from selenium.webdriver.support.color import Color
+from pymongo import MongoClient, ReturnDocument
+from datetime import datetime
+from BannerDetection import find_cookie_banner
 
-class CookieBanner:
-
-	def __init__(self, url, web_driver: webdriver):
-		self.web_driver = web_driver
-		self.url: url
-		self.approve_btn = None
-		self.reject_btn = None
-		self.more_btn = None
-
-
-	def spot_banner_buttons(self):
-		approve_word_keys = [
-			"accetto",
-			"sono d'accordo",
-			"accetta",
-			"accetta tutto",
-			"ok",
-			"accetta tutti"
-		]
-
-		deny_word_keys = [
-			"rifiuta",
-			"rifiuta tutto"
-			"rifiuto"
-			"rifiuta tutti"
-			"non sono d'accordo"
-			"continua senza accettare"
-		]
-		approve_btn = self.spot_a_btn(approve_word_keys)
-		deny_btn = self.spot_a_btn(deny_word_keys)
-
-		if approve_btn:
-			print("FIND APPROVE BUTTON")
-			approve_btn = BannerElement(approve_btn);
-			approve_btn.screenshot("approve")
-
-		if deny_btn:
-			print("FIND REJECT BUTTON")
-			deny_btn = BannerElement(deny_btn);
-			deny_btn.screenshot("deny")
+mainPath = os.path.abspath(os.getcwd())
 
 
-	def spot_a_btn(self, word_keys):
+class Database:
 
-		# looking for a button, an input field or a link
-		# since web sites banner not only use buttons
-		# for accept/reject cookie
-		elements_type = ["button", "input", "a"]
-		for type in elements_type:
-			elements = self.web_driver.find_elements(By.TAG_NAME, type)
+    def __init__(self, db_url=None):
+        try:
+            self.client = MongoClient(db_url)
+            self.db = self.client["CookieBanner"]
+            self.runs = self.db["runs"]
+            status = self.status()
+            log.debug(
+                "Running MongoDB {} on host {}.",
+                status["version"],
+                status["host"],
+                status["uptime"],
+            )
+        except Exception as e:
+            log.exception(e)
 
-			for element in elements:
+    def status(self):
+        try:
+            status = self.db.command("serverStatus")
+            return status
+        except Exception as e:
+            log.exception(e)
 
-				element_text = element.text or element.get_attribute("value")
-				if not element_text:
-					continue
-				element_text = "".join(char for char in element_text if char.isalpha())
-				element_text = "".join(element_text.lower().split())
+    def create_run(self, url):
+        try:
+            new_run = {
+                "url": url,
+                "status": "startingRun",
+            }
+            obj_id = self.runs.insert_one(new_run).inserted_id
+            return obj_id
+        except Exception as e:
+            log.exception(e)
+
+    def modify_run(self, run_id, data):
+        try:
+            run = self.runs.find_one_and_update(
+                {"_id": run_id}, {"$set": data}, return_document=ReturnDocument.AFTER
+            )
+            return run
+        except Exception as e:
+            log.exception(e)
+
+    def get_run(self, run_id):
+        try:
+            run = self.runs.find_one({"_id": run_id})
+            return run
+        except Exception as e:
+            log.exception(e)
 
 
-				for word_key in word_keys:
-					if word_key in element_text:
-						return element
-			print("Did not find any element.")
+class Button:
+    def __init__(
+            self, url, consent_elem: selenium.webdriver.remote.webelement.WebElement
+    ):
+        self.url = url
+        self.elem = consent_elem
+        self.size = self.elem.size
+        self.links = []
+        self.text = self.elem.text
+        self.apprBtn = None
+        self.apprBtnMeta = None
+        self.denyBtn = None
+        self.denyBtnMeta = None
+        self.moreBtn = None
+        self.moreBtnMeta = None
+        self.policyBtn = None
+        self.policyBtnMeta = None
+        self.html = self.elem.get_attribute("outerHTML")
+        for elem in self.elem.find_elements_by_partial_link_text(""):
+            self.links.append(elem.get_attribute("href"))
+        self.find_buttons()
+
+    def find_buttons(self):
+        preferences_word_keys = [
+            "gestiscipreferenze",
+            "gestisciopzioni",
+            "impostazionedeicookie",
+            "personalizzaleopzioni",
+            "gestiscilemieimpostazioni",
+            "gestisciimpostazioni",
+            "piùopzioni"
+        ]
+        moreBtn = self.spot_a_btn(preferences_word_keys)
+        if moreBtn:
+            log.debug("FOUND MORE BUTTON!")
+            moreBtn = ButtonElement(moreBtn)
+            self.moreBtn = moreBtn
+            self.moreBtnMeta = moreBtn.getMeta()
+
+        deny_word_keys = [
+            "rifiuta",
+            "rifiutatutto",
+            "rifiuto",
+            "rifiutatutti",
+            "nonsonod'accordo",
+            "continuasenzaaccettare",
+            "rifiutanonessenziali",
+        ]
+        denyBtn = self.spot_a_btn(deny_word_keys)
+        if denyBtn:
+            log.debug("FOUND REJECT BUTTON!")
+            denyBtn = ButtonElement(denyBtn)
+            self.denyBtn = denyBtn
+            self.denyBtnMeta = denyBtn.getMeta()
+
+        approve_word_keys = [
+            "accetto",
+            "sonod'accordo",
+            "accetta",
+            "accettatutto",
+            "ok",
+            "accettatutti",
+        ]
+        apprBtn = self.spot_a_btn(approve_word_keys)
+        if apprBtn:
+            log.debug("FOUND APPROVE BUTTON!")
+            apprBtn = ButtonElement(apprBtn)
+            self.apprBtn = apprBtn
+            self.apprBtnMeta = apprBtn.getMeta()
+        policy_word_keys=[
+            "privacy policy",
+        ]
+        policyBtn= self.spot_a_btn(policy_word_keys)
+        if policyBtn:
+            log.debug("FOUND POLICY LINK!")
+            policyBtn=ButtonElement(policyBtn)
+            self.policyBtn=policyBtn
+            self.policyBtnMeta=policyBtn.getMeta()
+        else:
+            policyBtn=self.spot_policy_link()
+            policyBtn = ButtonElement(policyBtn)
+            self.policyBtn = policyBtn
+            self.policyBtnMeta = policyBtn.getMeta()
+
+    @log.catch
+    def spot_a_btn(self, triggers):
+        elemTypes = ["button", "input", "a"]
+        for t in elemTypes:
+            elems = self.elem.find_elements_by_tag_name(t)
+            for elem in elems:
+                elemText = elem.text or elem.get_attribute("value")
+                if not elemText:
+                    continue
+                elemText = "".join(char for char in elemText if char.isalpha())
+                elemText = "".join(elemText.lower().split())
+                for trig in triggers:
+                    if trig == elemText:
+                        return elem
+        return None
+
+    @log.catch
+    def spot_policy_link(self):
+        xpaths = ["//a[contains(@href,'cookie')]", "//a[contains(@href,'policy')]"]
+        for xpath in xpaths:
+            elem = self.elem.find_element_by_xpath(xpath)
+            if elem:
+                log.debug("FOUND COOKIE POLICY LINK ELEMENT.")
+                return elem
+        log.debug("Did not find any cookie policy link.")
+        return None  # None found
+
+    def getMeta(self):
+        return {
+            "bannerSize": self.size,
+            "moreBtn": self.moreBtnMeta,
+            "denyBtn": self.denyBtnMeta,
+            "approveBtn": self.apprBtnMeta,
+            "cookiePolicy": self.policyBtnMeta,
+        }
 
 
-		return None
+class ButtonElement:
+
+    def __init__(self, btnElem: selenium.webdriver.remote.webelement.WebElement):
+        self.text = None
+        self.color = None
+        self.textColor = None
+        self.type = None
+        self.redirect = None
+        self.html = None
+        self.size = None
+        if btnElem:
+            self.elem = btnElem
+            if self.elem:
+                self.text = self.elem.text or self.elem.get_attribute("value")
+                self.type = self.elem.tag_name
+                self.html = self.elem.get_attribute("outerHTML")
+            if self.elem.size:
+                self.size = self.elem.size
+            if self.elem.get_attribute("href"):
+                self.redirect = self.elem.get_attribute("href")
+            if self.elem.value_of_css_property("background-color"):
+                self.color = Color.from_string(
+                    self.elem.value_of_css_property("background-color")
+                ).hex
+            if self.elem.value_of_css_property("color"):
+                self.textColor = Color.from_string(
+                    self.elem.value_of_css_property("color")
+                ).hex
+
+    def getMeta(self):
+        return {
+            "text": self.text,
+            "color": self.color,
+            "textColor": self.textColor,
+            "type": self.type,
+            "redirect": self.redirect,
+            "html": self.html,
+            "size": self.size,
+        }
 
 
-class BannerElement:
-	def __init__(self, element: WebElement):
-		self.element = element
+@log.catch
+class PageScanner:
+    def __init__(
+            self, browser: splinter.driver.DriverAPI, db: Database, url: str
+    ):
+        self.browser = browser
+        self.db = db
+        self.windowSize = self.browser.driver.get_window_size()
+        self.url = url
+        self.consent = None
 
-	def screenshot(self, name: str):
-		self.element.screenshot("screenshots/" + name + ".png")
+    @log.catch
+    def doScan(self):
+        try:
+            self.startedAt = datetime.now()
+            self.runId = self.db.create_run(self.url)
+            self.browser.cookies.delete()
+            self.browser.visit(self.url)
+
+            consent = find_cookie_banner(self.browser)
+            if consent:
+                self.consent = Button(self.url, consent)
+                self.endedAt = datetime.now()
+                self.db.modify_run(
+                    self.runId,
+                    {
+                        "status": "runDone",
+                        "browserSize": self.windowSize,
+                        "button": self.consent.getMeta(),
+                    },
+                )
+            else:
+                self.endedAt = datetime.now()
+                self.db.modify_run(
+                    self.runId,
+                    {
+                        "status": "noNoticeFound",
+                        "browserSize": self.windowSize,
+                    },
+                )
+                return None
+
+            log.info("DONE!")
+        except:
+            e = sys.exc_info()[0]
+            log.exception(e)
+
+
+
+def setupDriver(hless=False):
+    browserOptions = wd.ChromeOptions()
+    browserOptions.add_argument("--window-size=1920,1080")
+    browserOptions.add_experimental_option("w3c", False)
+    browserOptions.headless = hless
+    return Sbrowser("chrome", options=browserOptions)
+
+
 
--- main.py
+++ main.py
@@ -1,103 +1,27 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import *
+
+from loguru import logger as log
+from itertools import islice
 import csv
-from CookieBanner import CookieBanner, BannerElement
+from CookieBanner import Database, PageScanner, setupDriver
 
-SLEEP_TIME_CMP_WAIT = 3
+runId = 0
+if __name__ == "__main__":
+    db = Database()
+    url_list = []
+    with open("url_list_2.csv", "r") as link_csv_file:
+        csv_reader = csv.DictReader(link_csv_file)
 
+        header = next(csv_reader)
+        if header != None:
+            for link in islice(csv_reader, 5000):
+                http_string = "https://" + link["Domain"]
+                url_list.append(http_string)
 
-# def executeCmpFunction(it_list):
-#     csvreader = csv.reader(it_list)
-#     options = Options()
-#     options.add_argument("--headless")
-#     driver = webdriver.Chrome(options=options)
-#     visitedwebsites = []
-#
-#     for website_name in csvreader:
-#         url = "https://" + website_name
-#         driver.get(url)
-#         time.sleep(SLEEP_TIME_CMP_WAIT)
-#
-#         try:
-#             driver.execute_script('if (__tcf) return "ok";', None)
-#             print('__tcf found')
-#             cookie_banner = CookieBanner(url, driver)
-#             cookie_banner.spot_banner_buttons()
-#             #website.tcfexists = True
-#         except JavascriptException as e:
-#             print('__tcf not found: ', e)
-#
-#     print(visitedwebsites)
+    for url in url_list:
+        runId += 1
+        browser = setupDriver(True)
+        with log.contextualize(url=url):
+            res = PageScanner(browser, db, url)
+            res.doScan()
+            browser.quit()
 
-def checkTCFandFindButtons(it_list):
-    tcfwebsitefile = 'tcfwebsites.csv'
-    csvreader = csv.reader(it_list)
-    tcfwebsites = []
-
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome(options=options)
-
-    try:
-        for row in csvreader:
-            str = row[0]
-            print(str)
-            try:
-                time.sleep(SLEEP_TIME_CMP_WAIT)
-                driver.get("https://" + str)
-            except WebDriverException as e:
-                print(e.stacktrace)
-            try:
-                driver.execute_script('if (__tcfapi) return "ok";', None)
-                print('__tcf found')
-                tcfwebsites.append(str)
-            except Exception as e:
-                continue
-            try:
-                with open(tcfwebsitefile, 'w', newline='') as fl:
-                    writer = csv.writer(fl)
-                    for website in tcfwebsites:
-                        writer.writerow([website])
-            except BaseException as e:
-                print('BaseException:', fl)
-    # website.tcfexists = True
-    except JavascriptException as e:
-        print('__tcf not found: ', e)
-
-
-# cookie_banner = CookieBanner(driver)
-# cookie_banner.spot_banner_buttons()
-
-
-# takes as input the tranco list with the top 1M websites
-# and creates a csv with only italian domain web sites
-def filterList(tranco_list):
-    itwebsitefile = 'itwebsites.csv'
-    csvreader = csv.reader(tranco_list)
-    italianwebsites = []
-
-    try:
-        for row in csvreader:
-            str = row[1]
-            if str.endswith('.it'):
-                italianwebsites.append(str)
-
-        try:
-            with open(itwebsitefile, 'w', newline='') as fl:
-                writer = csv.writer(fl)
-                for website in italianwebsites:
-                    writer.writerow([website])
-
-        except BaseException as e:
-            print('BaseException:', fl)
-
-    except BaseException as e:
-        print('BaseException:', tranco_list)
-
-
-# tranco_list = open('tranco_25JP9.csv')
-# filterList(tranco_list)
-it_list = open('itwebsites.csv')
-checkTCFandFindButtons(it_list)
