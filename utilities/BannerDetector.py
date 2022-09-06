a58675952109cb9466c4d3f7097355c74c8f64ca
martinaburo <m.buro2@studenti.unimol.it>
2022-09-07 01:37:42 +0200

    Fixing code

--- utilities/BannerDetector.py
+++ utilities/BannerDetector.py
@@ -1,4 +1,3 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
--- utilities/ButtonResearch.py
+++ utilities/ButtonResearch.py
@@ -1,14 +1,11 @@
 import sys
 import os
-import splinter
-import selenium
+from selenium import webdriver
 from loguru import logger as log
-from splinter import Browser as Sbrowser
-from selenium import webdriver as wd
-from selenium.webdriver.support.color import Color
+from selenium.webdriver.common.by import By
 from pymongo import MongoClient, ReturnDocument
 from datetime import datetime
-from utilities.ButtonDetection import find_cookie_banner
+from utilities.ButtonElement import ButtonElement
 
 mainPath = os.path.abspath(os.getcwd())
 
@@ -67,13 +64,12 @@ class Database:
 
 class Button:
     def __init__(
-            self, url, consent_elem: selenium.webdriver.remote.webelement.WebElement
+            self, url: str, web_driver: webdriver, db: Database
     ):
         self.url = url
-        self.elem = consent_elem
-        self.size = self.elem.size
+        self.db = db
+        self.web_driver = web_driver
         self.links = []
-        self.text = self.elem.text
         self.apprBtn = None
         self.apprBtnMeta = None
         self.denyBtn = None
@@ -84,10 +80,6 @@ class Button:
         self.moreBtnMeta = None
         self.policyBtn = None
         self.policyBtnMeta = None
-        self.html = self.elem.get_attribute("outerHTML")
-        for elem in self.elem.find_elements_by_partial_link_text(""):
-            self.links.append(elem.get_attribute("href"))
-        self.find_buttons()
 
     def find_buttons(self):
         preferences_word_keys = [
@@ -101,13 +93,14 @@ class Button:
             "gestionedelmonitoraggio",
             "impostazionicookie",
             "personalizza",
+            "preferenzedeicookie",
         ]
-        moreBtn = self.spot_a_btn(preferences_word_keys)
+        moreBtn = self.button_types(preferences_word_keys)
         if moreBtn:
             log.debug("FOUND MORE BUTTON!")
             moreBtn = ButtonElement(moreBtn)
             self.moreBtn = moreBtn
-            self.moreBtnMeta = moreBtn.getMeta()
+            self.moreBtnMeta = moreBtn.getDataButton()
 
         deny_word_keys = [
             "rifiuta",
@@ -120,22 +113,27 @@ class Button:
             "nonaccettareechiudi",
             "nonaccettare"
         ]
-        deny_word_keys_ambiguous=[
+        deny_word_keys_ambiguous = [
             "salvaedesci",
             "salvalemiescelte",
+            "salva",
         ]
-        denyBtn = self.spot_a_btn(deny_word_keys)
-        denyBtnAmbiguous = self.spot_a_btn(deny_word_keys_ambiguous)
+        denyBtn = self.button_types(deny_word_keys)
+        denyBtnAmbiguous = self.button_types(deny_word_keys_ambiguous)
         if denyBtn:
             log.debug("FOUND REJECT BUTTON!")
             denyBtn = ButtonElement(denyBtn)
             self.denyBtn = denyBtn
-            self.denyBtnMeta = denyBtn.getMeta()
-        else:
+            self.denyBtnMeta = denyBtn.getDataButton()
+        elif denyBtnAmbiguous:
             log.debug("FOUND REJECT BUTTON WITH AMBIGUOUS TEXT")
             denyBtnAmbiguous = ButtonElement(denyBtnAmbiguous)
             self.denyBtnAmbiguous = denyBtnAmbiguous
-            self.denyBtnAmbiguousMeta = denyBtnAmbiguous.getMetaAmbiguous()
+            self.denyBtnAmbiguousMeta = denyBtnAmbiguous.getDataAmbiguous()
+        elif moreBtn:
+            log.debug("click")
+            # cookie_button = self.elem.find_element_by_xpath("//button[text()='Preferenze dei cookie']")
+            # cookie_button.click()
 
         approve_word_keys = [
             "accetto",
@@ -146,173 +144,95 @@ class Button:
             "accettatutti",
             "accettoilmonitoraggio",
             "accettaechiudi",
+            "accettatuttiicookie",
         ]
-        apprBtn = self.spot_a_btn(approve_word_keys)
+        apprBtn = self.button_types(approve_word_keys)
         if apprBtn:
             log.debug("FOUND APPROVE BUTTON!")
             apprBtn = ButtonElement(apprBtn)
             self.apprBtn = apprBtn
-            self.apprBtnMeta = apprBtn.getMeta()
-        policy_word_keys=[
+            self.apprBtnMeta = apprBtn.getDataButton()
+        policy_word_keys = [
             "privacy policy",
         ]
-        policyBtn= self.spot_a_btn(policy_word_keys)
+        policyBtn = self.button_types(policy_word_keys)
         if policyBtn:
-            log.debug("FOUND POLICY BUTTON!")
-            policyBtn=ButtonElement(policyBtn)
-            self.policyBtn=policyBtn
-            self.policyBtnMeta=policyBtn.getMeta()
-        else:
-            policyBtn=self.spot_policy_link()
+            log.debug("FOUND COOKIE POLICY BUTTON!")
             policyBtn = ButtonElement(policyBtn)
             self.policyBtn = policyBtn
-            self.policyBtnMeta = policyBtn.getMeta()
+            self.policyBtnMeta = policyBtn.getDataButton()
+        else:
+            policyBtn = self.policy_link()
+            policyBtn = ButtonElement(policyBtn)
+            self.policyBtn = policyBtn
+            self.policyBtnMeta = policyBtn.getDataButton()
 
     @log.catch
-    def spot_a_btn(self, triggers):
+    def button_types(self, word_keys):
         elemTypes = ["button", "input", "a"]
-        for t in elemTypes:
-            elems = self.elem.find_elements_by_tag_name(t)
+        for type in elemTypes:
+            elems = self.web_driver.find_elements(By.TAG_NAME, type)
             for elem in elems:
                 elemText = elem.text or elem.get_attribute("value")
                 if not elemText:
                     continue
                 elemText = "".join(char for char in elemText if char.isalpha())
                 elemText = "".join(elemText.lower().split())
-                for trig in triggers:
-                    if trig == elemText:
+                for word_key in word_keys:
+                    if word_key == elemText:
                         return elem
         return None
 
     @log.catch
-    def spot_policy_link(self):
+    def policy_link(self):
         xpaths = ["//a[contains(@href,'cookie')]", "//a[contains(@href,'policy')]"]
         for xpath in xpaths:
-            elem = self.elem.find_element_by_xpath(xpath)
+            elem = self.web_driver.find_element_by_xpath(xpath)
             if elem:
-                log.debug("FOUND COOKIE POLICY LINK ELEMENT.")
+                log.debug("FOUND COOKIE POLICY LINK ELEMENT!.")
                 return elem
-        log.debug("Did not find any cookie policy link.")
-        return None  # None found
+        return None
 
-    def getMeta(self):
+    def getData(self):
         return {
-            "bannerSize": self.size,
             "moreBtn": self.moreBtnMeta,
             "denyBtn": self.denyBtnMeta,
             "approveBtn": self.apprBtnMeta,
             "cookiePolicy": self.policyBtnMeta,
         }
 
-
-class ButtonElement:
-
-    def __init__(self, btnElem: selenium.webdriver.remote.webelement.WebElement):
-        self.text = None
-        self.color = None
-        self.textColor = None
-        self.type = None
-        self.redirect = None
-        self.html = None
-        self.size = None
-        if btnElem:
-            self.elem = btnElem
-            if self.elem:
-                self.text = self.elem.text or self.elem.get_attribute("value")
-                self.type = self.elem.tag_name
-                self.html = self.elem.get_attribute("outerHTML")
-            if self.elem.size:
-                self.size = self.elem.size
-            if self.elem.get_attribute("href"):
-                self.redirect = self.elem.get_attribute("href")
-            if self.elem.value_of_css_property("background-color"):
-                self.color = Color.from_string(
-                    self.elem.value_of_css_property("background-color")
-                ).hex
-            if self.elem.value_of_css_property("color"):
-                self.textColor = Color.from_string(
-                    self.elem.value_of_css_property("color")
-                ).hex
-
-    def getMeta(self):
-        return {
-            "text": self.text,
-            "ambiguousText": False,
-            "color": self.color,
-            "textColor": self.textColor,
-            "type": self.type,
-            "redirect": self.redirect,
-            "html": self.html,
-            "size": self.size,
-        }
-    def getMetaAmbiguous(self):
-        return {
-            "text": self.text,
-            "ambiguousText": True,
-            "color": self.color,
-            "textColor": self.textColor,
-            "type": self.type,
-            "redirect": self.redirect,
-            "html": self.html,
-            "size": self.size,
-        }
-
-
-@log.catch
-class PageScanner:
-    def __init__(
-            self, browser: splinter.driver.DriverAPI, db: Database, url: str
-    ):
-        self.browser = browser
-        self.db = db
-        self.windowSize = self.browser.driver.get_window_size()
-        self.url = url
-        self.consent = None
-
     @log.catch
-    def doScan(self):
+    def doResearch(self):
         try:
             self.startedAt = datetime.now()
             self.runId = self.db.create_run(self.url)
-            self.browser.cookies.delete()
-            self.browser.visit(self.url)
-
-            consent = find_cookie_banner(self.browser)
-            if consent:
-                self.consent = Button(self.url, consent)
+            self.web_driver.delete_all_cookies()
+            self.web_driver.get(self.url)
+            self.find_buttons()
+            cookies = self.getData()
+            if cookies:
                 self.endedAt = datetime.now()
                 self.db.modify_run(
                     self.runId,
                     {
                         "status": "runDone",
-                        "browserSize": self.windowSize,
-                        "button": self.consent.getMeta(),
-                    },
+                        "button": cookies
+                    }
                 )
             else:
                 self.endedAt = datetime.now()
                 self.db.modify_run(
                     self.runId,
                     {
-                        "status": "noNoticeFound",
-                        "browserSize": self.windowSize,
+                        "status": "noDataFound",
                     },
                 )
                 return None
-
             log.info("DONE!")
         except:
             e = sys.exc_info()[0]
             log.exception(e)
 
-
-
-def setupDriver(hless=False):
-    browserOptions = wd.ChromeOptions()
-    browserOptions.add_argument("--window-size=1920,1080")
-    browserOptions.add_experimental_option("w3c", False)
-    browserOptions.headless = hless
-    return Sbrowser("chrome", options=browserOptions)
 
 
 
